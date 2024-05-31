0dbcafaf9d5651a924be673a33e58ce01e9b0e66
dJonaitis <55477640+dJonaitis@users.noreply.github.com>
2024-05-31 12:01:48 +0200

    Mobile navbar fix

--- templates/landing.js
+++ templates/landing.js
@@ -38,3 +38,14 @@ document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('resize', checkScroll);
     checkScroll(); // Initial check on page load
 });
+
+let navbarOpened = false;
+function toggleLandingTitle(){
+    navbarOpened = !navbarOpened;
+    if(navbarOpened){
+        document.getElementById("landingTitle").style.display = "none";
+    } else{
+        document.getElementById("landingTitle").style.display = "block";
+    }
+}
+
